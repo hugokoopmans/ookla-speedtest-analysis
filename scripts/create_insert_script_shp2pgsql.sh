2f25a18842e7849d2eca801b791135ac7d0028dd
Erik van der Hoeven <Erik.Hoeven@gmail.com>
2015-04-21 22:02:31 +0200

    final version for T-Mobile

--- scripts/create_insert_script_shp2pgsql.sh
+++ scripts/create_insert_script_shp2pgsql.sh
@@ -1,6 +1,6 @@
 rmdir --ignore-fail-on-non-empty ../loadscripts
 mkdir ../loadscripts
-shp2pgsql -s 28992 -d -I -g geom -W "LATIN1" ./shape-2013-versie-1.0/gem_2013_v1.shp nlstaging.gem_2013 > ../loadscripts/insert_gem_2013_v1.sql
-shp2pgsql -s 28992 -d -I -g geom -W "LATIN1" ./shape-2013-versie-1.0/buurt_2013_v1.shp nlstaging.gem_2013 > ../loadscripts/insert_buurt_2013_v1.sql
-shp2pgsql -s 28992 -d -I -g geom -W "LATIN1" ./shape-2013-versie-1.0/wijk_2013_v1.shp nlstaging.gem_2013 > ../loadscripts/insert_wijk_2013_v1.sql
+shp2pgsql -s 28992 -d -I -g geom -W "LATIN1" ./shape-2014-versie-1.0/gem_2014.shp nlstaging.gem_2014 > ../loadscripts/insert_gem_2014.sql
+shp2pgsql -s 28992 -d -I -g geom -W "LATIN1" ./shape-2014-versie-1.0/buurt_2014.shp nlstaging.gem_2014 > ../loadscripts/insert_buurt_2014.sql
+shp2pgsql -s 28992 -d -I -g geom -W "LATIN1" ./shape-2014-versie-1.0/wijk_2014.shp nlstaging.gem_2014 > ../loadscripts/insert_wijk_2014.sql
 
--- scripts/insert_pgdata.sh
+++ scripts/insert_pgdata.sh
@@ -1,4 +1,4 @@
 rm ../loadscripts/*.log
-psql -U hugo -d 4Gdb -v schema=nlstaging,public -f ../loadscripts/insert_gem_2013_v1.sql -L ../loadscripts/insert_gem_2013_v1.log 
+psql -U hugo -d 4Gdb -v schema=nlstaging,public -f ../loadscripts/insert_gem_2014.sql -L ../loadscripts/insert_gem_2014.log 
 
 
