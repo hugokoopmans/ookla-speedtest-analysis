7e7920382529c0ff01379150394e36446274c0f9
Erik van der Hoeven <Erik.Hoeven@gmail.com>
2015-04-25 16:08:44 +0200

    Geration test 5 for final version Q1 2015

---
+++ Global.R
@@ -0,0 +1,9 @@
+lsQuarterMonths <- c("January","February","March")
+year <- 2015
+QuarterNumber <- "Q1"
+
+dbName <- "4Gdb"
+hostName   <- "localhost"
+dbUserName <- "hugo"
+dbPassword <- "hugo"
+Portnumber <- 5432
