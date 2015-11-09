d40dbd03d7aab983d91a5aee1e0fb40b84275e17
hugo <hugo.koopmans@dikw.com>
2015-11-09 14:19:12 -0400

    fixed square km instead of hectaresin CBS data

--- global.R
+++ global.R
@@ -1,8 +1,8 @@
 # all parameters should go in here
 # Analysis period details
-p.ListQuarterMonths <- c("April","May","June")
+p.ListQuarterMonths <- c("July","August","September")
 p.Year <- 2015
-p.QuarterNumber <- "Q2"
+p.QuarterNumber <- "Q3"
 
 # PostgresSQL database details
 db.name <- "4Gdb"
